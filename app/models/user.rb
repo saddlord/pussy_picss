a3ff86e5e96b58e1f8e68ee0a78cb7c506b004c1
assouev <bsngr.asv@gmail.com>
2019-11-25 17:50:59 +0100

    page index, ect

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :cart       
-  has_many :orders       
+
+  has_one :cart       
+  has_many :orders     
 end
